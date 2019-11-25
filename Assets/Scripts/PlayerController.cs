8407308f9ab142549c209ab477882b4ddf7a20c1
Carlos José Bulado <carlosbulado@gmail.com>
2019-11-25 10:50:52 -0500

    Add player movement left, right, up, down.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    public float moveSpeed;
+    // Variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalMovement = Input.GetAxisRaw("Horizontal");
+        var verticalMovement = Input.GetAxisRaw("Vertical");
+        if(horizontalMovement > 0.5f || horizontalMovement < -0.5f)
+        {
+            transform.Translate(new Vector3(horizontalMovement * this.moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if(verticalMovement > 0.5f || verticalMovement < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, verticalMovement * this.moveSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
