71646a38d5b2b746230876e539770afb4e4fed01
Carlos Jose Bulado <carlosbulado@gmail.com>
2019-11-25 20:53:01 -0500

    Add camera movement and its following the player around

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Variables
+    public GameObject followTarget;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+    //Variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, this.targetPosition, this.moveSpeed * Time.deltaTime);
+    }
+}
