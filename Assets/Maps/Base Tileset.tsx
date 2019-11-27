f15a272d8f6a39419d699bf68f5cc3578365ff00
Carlos José Bulado <carlosbulado@gmail.com>
2019-11-26 21:48:48 -0500

    Add collision and automatic layer depth

--- Assets/Maps/Base Tileset.tsx
+++ Assets/Maps/Base Tileset.tsx
@@ -1,4 +1,112 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="Base Tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
  <image source="../Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.0739486" y="0" width="15.899" height="15.9729"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0739486" y="-0.0739486" width="15.825" height="16.0469"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="6">
+   <object id="6" x="0.961332" y="0.961332" width="15.0855" height="15.0855"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.147897" y="0.813435" width="16.1208" height="15.3813"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0739486" y="0.961332" width="15.0116" height="15.0855"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="-0.0739486" y="-0.147897" width="15.9729" height="12.0536"/>
+   <object id="5" x="0.0739486" y="11.9057" width="11.7578" height="4.14112"/>
+   <object id="6" x="9.61332" y="10.2049" width="4.43692" height="3.84533"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.887384" y="-0.147897" width="15.1595" height="16.2687"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.0469" height="15.9729"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0739486" width="15.0855" height="15.899"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="5">
+   <object id="6" x="0.887384" y="-0.147897" width="15.1595" height="15.0855"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.147897" y="-0.0739486" width="16.0469" height="15.0116"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14.9376" height="14.9376"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="640">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0739486" y="0" width="15.9729" height="15.9729"/>
+  </objectgroup>
+ </tile>
+ <tile id="641">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0739486" width="16.0469" height="15.899"/>
+  </objectgroup>
+ </tile>
+ <tile id="642">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0739486" width="15.9729" height="15.899"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0739486" y="0.0739486" width="16.0469" height="15.899"/>
+  </objectgroup>
+ </tile>
+ <tile id="644">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0739486" y="0.221846" width="15.825" height="15.7511"/>
+  </objectgroup>
+ </tile>
+ <tile id="645">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0739486" y="-0.0739486" width="16.1208" height="15.9729"/>
+  </objectgroup>
+ </tile>
 </tileset>
---
+++ Assets/Maps/red_collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="red_collision" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../../../Downloads/GamesPlusJames - Unity 2D RPG/red-collision-box.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0739486" width="15.9729" height="15.825"/>
+  </objectgroup>
+ </tile>
+</tileset>
