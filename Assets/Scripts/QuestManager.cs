ed0d709a4b51ce06226ccb6d216c61522c907a61
Carlos José Bulado <carlosbulado@gmail.com>
2019-12-01 14:08:37 -0500

    Add quest manager and quest objects.

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // Variables
+    public QuestObject[] allQuests;
+    public bool[] questsCompleted;
+
+    public DialogManager dialogManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.questsCompleted = new bool[this.allQuests.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowQuestDialog(string[] lines)
+    {
+        this.dialogManager.ShowDialog(lines);
+    }
+}
---
+++ Assets/Scripts/QuestObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour
+{
+    // Variables
+    public QuestManager questManager;
+    public int questNumber;
+
+    public string[] startQuestDialog;
+    public string[] completeQuestDialog;
+    public string[] afterCompleteQuestDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartQuest()
+    {
+        this.questManager.ShowQuestDialog(this.startQuestDialog);
+    }
+
+    public void CompleteQuest()
+    {
+        this.questManager.questsCompleted[this.questNumber] = true;
+        gameObject.SetActive(false);
+        this.questManager.ShowQuestDialog(this.completeQuestDialog);
+    }
+}
