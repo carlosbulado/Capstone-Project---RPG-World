c4123f88a14c5c0f159d76dccb70bc7e6ff1b460
Carlos José Bulado <carlosbulado@gmail.com>
2019-12-01 23:42:06 -0500

    Add volume controller and manager.

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+    // Variables
+    protected AudioSource audio;
+    protected float audioLevel;
+    public float defaultAudioLevel;
+
+    // Setters
+    public void SetAudioLevel(float value)
+    {
+        if(this.audioLevel == null) { this.audio = GetComponent<AudioSource>(); }
+
+        this.audioLevel = this.defaultAudioLevel * value;
+        this.audio.volume = this.audioLevel;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour
+{
+    // Variables
+    protected VolumeController[] allAudios;
+    public float currentVolumeLevel;
+    protected float maxVolumeLevel = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.allAudios = FindObjectsOfType<VolumeController>();
+
+        if(this.currentVolumeLevel > this.maxVolumeLevel)
+        {
+            this.currentVolumeLevel = this.maxVolumeLevel;
+        }
+
+        foreach (VolumeController audio in this.allAudios)
+        {
+            audio.SetAudioLevel(this.currentVolumeLevel);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (VolumeController audio in this.allAudios)
+        {
+            audio.SetAudioLevel(this.currentVolumeLevel);
+        }
+    }
+}
