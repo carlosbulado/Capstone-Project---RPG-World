fba184900cd023581d7a77794ff81bfbefb29a2b
Carlos José Bulado <carlosbulado@gmail.com>
2019-12-01 23:19:28 -0500

    Add  music manager.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Variables
+    protected static bool musicManagerExists;
+    public AudioSource[] musicTracks;
+    public int currentTrack;
+    public bool musicCanPlay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Fix the duplicates of MusicManager in the world
+        if(!MusicManager.musicManagerExists)
+        {
+            MusicManager.musicManagerExists = true;
+            // This code will make sure that this manager will not be destroyed when change scenes
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource music = this.musicTracks[this.currentTrack];
+        if(this.musicCanPlay)
+        {
+            if(!music.isPlaying)
+            {
+                for (int i = 0; i < this.musicTracks.Length; i++)
+                {
+                    AudioSource _music = this.musicTracks[i];
+                    if(i != this.currentTrack)
+                    {
+                        _music.Stop();
+                    }
+                }
+                music.Play();
+            }
+        }
+        else
+        {
+            foreach (AudioSource _music in this.musicTracks)
+            {
+                _music.Stop();
+            }
+        }
+    }
+
+    public void SwitchTrack(int newTrack)
+    {
+        this.currentTrack = newTrack;
+    }
+}
---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour
+{
+    // Variables
+    protected MusicManager musicManager;
+    public int newTrack;
+    public bool switchOnStart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.musicManager = FindObjectOfType<MusicManager>();
+
+        if(this.switchOnStart)
+        {
+            this.musicManager.SwitchTrack(this.newTrack);
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            this.musicManager.SwitchTrack(this.newTrack);
+            gameObject.SetActive(false);
+        }
+    }
+}
